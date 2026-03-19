ef114cfd07219a68e97bc134883fc80e8017d06b
cortezmarjho-gif <ac0427032026@unab.edu.sv>
2026-03-19 17:49:51 -0600

    se agrega una retroalimentacio de los temas que vimos en semana9

---
+++ semana7/retroAlimentacionSemana9.py
@@ -0,0 +1,49 @@
+serie = "Fullmetal alchemits"
+
+## es un arreglo es una variablo que tiene adentro otra variable
+
+## cuando una variable cambia => pierde inutabilidad
+## POO
+## polimorfismo -> es el cambio de acciones sin que se rompa el codigo 
+## adtracciones ->
+##   tasa de cafe
+##   cafe coscafe
+#    azucar
+##   otros ingredientes
+##   un objeto es el que toma un odeloby este modelo le da funcones y utiliza sus propiedades
+
+## Leon ->
+## tiene ojos (propiedades)
+## tiene boca
+## esta guapo 
+###############
+# corre (fuciones)
+#salta
+
+##clases
+## estructura de datos
+
+## Listas.
+## Arrays.
+## tuplas.
+## indices.
+
+
+#------------------------------------------------------------------
+def saludo (nombres):
+    print (nombres)
+#saludo(serie)las funciones siempre van a tener ()
+#------------------------------------------------------------------
+## las funciones tienen un espacio
+## scope es donde reciden las variables
+
+##colocar el nombre de la serie como titulo
+fmaTemu = serie.title()
+saludo(serie)
+#saludo(fmaTemu)
+fnaMayusculas = serie.upper()
+saludo (fnaMayusculas)
+#  deprogracion lineal
+
+FullmetalCapitalizer = serie.capitalize()
+saludo(FullmetalCapitalizer)
