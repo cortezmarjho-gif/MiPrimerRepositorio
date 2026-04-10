4b519dec015d781f1189bbff6b114fd14f4aba24
cortezmarjho-gif <ac0427032026@unab.edu.sv>
2026-04-09 22:16:24 -0600

    Parcial 2 Fundamentos de programacion - Ejercicio 2

---
+++ parcial2ejercicio2.py
@@ -0,0 +1,4 @@
+texto = "marjhory odalis alvarez cortez"
+texto_cambiado = texto.title()
+texto_nuevo = texto_cambiado.replace("Marjhory Odalis Alvarez Cortez", "Cortez")
+print("Texto final: ", texto_nuevo)
