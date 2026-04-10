b59f58191d466ecd30b9a286db19ab00390f33c3
cortezmarjho-gif <ac0427032026@unab.edu.sv>
2026-04-09 22:19:39 -0600

    Parcial 2 Fundamentos de programacion - Ejercicio 8

---
+++ parcial2ejercicio8.py
@@ -0,0 +1,7 @@
+texto = """Amor eterno
+alma que canta
+abriga el alma"""
+a_cantidad = texto.count("a")
+lineas = texto.splitlines()
+print("Cantidad de 'a':", a_cantidad)
+print("Numero de lineas:", lineas)
