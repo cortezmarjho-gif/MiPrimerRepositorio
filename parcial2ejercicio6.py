47169543e635a460fb4674e019acf23499906c3b
cortezmarjho-gif <ac0427032026@unab.edu.sv>
2026-04-09 22:18:59 -0600

    Parcial 2 Fundamentos de programacion - Ejercicio 6

---
+++ parcial2ejercicio6.py
@@ -0,0 +1,5 @@
+texto = "marjhory odalis alvarez cortez"
+normalizado = texto.casefold()
+alfabetico = normalizado.isalpha()
+print("Texto normalizado:", normalizado)
+print("¿Solo contiene letras?:", alfabetico)
