18a1cec76334c0177a73a0c3220edddc6238b51b
cortezmarjho-gif <ac0427032026@unab.edu.sv>
2026-04-18 11:24:51 -0600

    Uso de IF, For, While ejercicio 2

---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+edad = int (input("Ingrese su edad: "))
+
+if edad < 18:
+    print("Eres menor de edad.")
+
+elif edad >= 60:
+    print("Eres adulto mayor.")
+else:
+    print("Eres mayor de edad.")
